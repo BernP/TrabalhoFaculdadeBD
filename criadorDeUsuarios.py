6fc480c4cc6bff2f2dd576fc88cf90ce2d0f9031
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2024-08-29 19:50:27 -0300

    a

--- criadorDeUsuarios.py
+++ criadorDeUsuarios.py
@@ -6,7 +6,7 @@ from random import randint
 
 
 def CriarNomeAleatorio():
-    primeirosNome = ['Augusto', 'João', 'José', 'Ricardo', 'Felipe', 'Lucas', 'Igor', 'Arthur', 'Artur', 'André', 'Anderson', 'Eduardo', 'Bruno', 'Mario', 'Maria', 'Maria Eduarda', 'Helena', 'Filipa','Felícia', 'Lara','Luana','Letícia','Cecília','Marina','Mariana','Carolina','Raquel']
+    primeirosNome = ['Augusto', 'Joao', 'Jose', 'Ricardo', 'Felipe', 'Lucas', 'Igor', 'Arthur', 'Artur', 'Andre', 'Anderson', 'Eduardo', 'Bruno', 'Mario', 'Maria', 'Maria Eduarda', 'Helena', 'Filipa','Felicia', 'Lara','Luana','Leticia','Cecilia','Marina','Mariana','Carolina','Raquel']
     sobreNomes = ['Silva', 'da Silva', 'Cruz', 'da Cruz', 'Carvalho', 'Pereira', 'Silveira', 'Figueredo', 'Costa','da Costa', 'Ferreira', 'Lima', 'Santos', 'Mendes', 'Nunes','Teixeira', 'Marques', 'Alves', 'Gomes', 'Lopes', 'Sousa', 'Souza']
     
     nome = primeirosNome[randint(0, len(primeirosNome)-1)] + ' '
@@ -14,13 +14,13 @@ def CriarNomeAleatorio():
     
     for x in range(quantidadeDeSobrenomes):
         escolhido = randint(0,len(sobreNomes)-1)
-        if x < quantidadeDeSobrenomes:
+        if x < quantidadeDeSobrenomes-1:
             nome += sobreNomes[escolhido] + ' '
             sobreNomes.pop(escolhido)
         else:
             nome += sobreNomes[escolhido]
             sobreNomes.pop(escolhido)
-    
+
     return(nome)
 
 def CriarCepAleatorio():
@@ -52,7 +52,7 @@ def CriarCPFAleatorio():
     return cpf
 
 def CriarEndereçoAleatorio():
-    primeirosNome = ['Augusto', 'João', 'José', 'Ricardo', 'Felipe', 'Lucas', 'Igor', 'Arthur', 'Artur', 'André', 'Anderson', 'Eduardo', 'Bruno', 'Mario', 'Maria', 'Maria Eduarda', 'Helena', 'Filipa','Felícia', 'Lara','Luana','Letícia','Cecília','Marina','Mariana','Carolina','Raquel']
+    primeirosNome = ['Augusto', 'Joao', 'Jose', 'Ricardo', 'Felipe', 'Lucas', 'Igor', 'Arthur', 'Artur', 'Andre', 'Anderson', 'Eduardo', 'Bruno', 'Mario', 'Maria', 'Maria Eduarda', 'Helena', 'Filipa','Felicia', 'Lara','Luana','Leticia','Cecilia','Marina','Mariana','Carolina','Raquel']
     sobreNomes = ['Silva', 'da Silva', 'Cruz', 'da Cruz', 'Carvalho', 'Pereira', 'Silveira', 'Figueredo', 'Costa','da Costa', 'Ferreira', 'Lima', 'Santos', 'Mendes', 'Nunes','Teixeira', 'Marques', 'Alves', 'Gomes', 'Lopes', 'Sousa', 'Souza']
     
 
@@ -68,10 +68,10 @@ def CriarEndereçoAleatorio():
             nome += sobreNomes[escolhido]
             sobreNomes.pop(escolhido)
 
-    epipeto = ['Marechal', 'Comandante', 'Capitão', 'Professor(a)', 'Presidente', 'Desembargador(a)', 'Sr(a).']
-    lugares = ['Praça', 'Rua', 'Avenida']
+    epipeto = ['Marechal', 'Comandante', 'Capitao', 'Professor(a)', 'Presidente', 'Desembargador(a)', 'Sr(a).']
+    lugares = ['Praca', 'Rua', 'Avenida']
 
-    endereco = lugares[randint(0, len(lugares)-1)] + ' ' + epipeto[randint(0, len(epipeto)-1)] + ' '+ nome +'- número '+ str(randint(1,1200))
+    endereco = lugares[randint(0, len(lugares)-1)] + ' ' + epipeto[randint(0, len(epipeto)-1)] + ' '+ nome +'- numero '+ str(randint(1,1200))
     
     if(randint(0,9) >= 8):
         endereco += ' apt. ' + chr(randint(0,9)+48)+chr(randint(0,5)+48)+chr(randint(1,9)+48)
@@ -84,7 +84,11 @@ def CriarEndereçoAleatorio():
 #print(CriarEndereçoAleatorio())
 
 
-quantidadeDeUsuarios = 10
+quantidadeDeUsuarios = 2000
+arquivo = open('comando_insert.txt', 'w+')
+
+
 for x in range(quantidadeDeUsuarios):
-    codigoParaInserir = "INSERT INTO USUARIOS (nome, cpf, cep, endereco, pedidos, id) VALUES (" + "'"+CriarNomeAleatorio()+ ",'" + ' '+ "'" + CriarCepAleatorio()+ "'," + ' '+ "'"+ CriarCPFAleatorio()+ "'," + ' '+ "'" + CriarEndereçoAleatorio()+ "', [], "+ str(x) +')'
-    print(codigoParaInserir)
+    codigoParaInserir = "INSERT INTO USUARIOS (nome, cpf, cep, endereco, id) VALUES (" + "'"+CriarNomeAleatorio()+ "'," + ' '+ "'" + CriarCPFAleatorio()+ "'," + ' '+ "'"+  CriarCepAleatorio() + "'," + ' '+ "'" + CriarEndereçoAleatorio()+ "', "+ str(x) +');\n'
+    arquivo.write(codigoParaInserir)
+arquivo.close()
